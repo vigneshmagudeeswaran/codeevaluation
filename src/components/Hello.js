d5dc3da022ce6f97c7a02b91a0efed5a9bc68d1f
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-13 02:37:37 +0530

    JSXVSNormalJavaScript

---
+++ src/components/Hello.js
@@ -0,0 +1,17 @@
+import React from "react";
+// Example for JSX
+const Hello = () => {
+  return (
+    <div>
+      <h1>Hello Vishwas</h1>
+    </div>
+  );
+};
+
+// Example for normal java script
+// return React.createElement(
+// 'div',
+//     null,
+//     React.createElement('h1', null, 'Hello Vishwas')
+// )
+export default Hello;
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,6 @@ class Welcome extends Component {
     render() {
         return <h1>Class Component</h1>
     }
-
 }
 
 export default Welcome
