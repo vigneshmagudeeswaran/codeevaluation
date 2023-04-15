24cd8977c9fad3550dcaf84ddf231bd498e872f9
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-15 08:54:21 +0530

    Index_as_Key_Anti-pattern

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -22,7 +22,9 @@ function NameList() {
       skill: "Vue",
     },
   ];
-  const personList = persons.map((person) => <Person person={person}></Person>);
+  const personList = persons.map((person) => (
+    <Person key={person.id} person={person}></Person>
+  ));
   return <div>{personList}</div>;
 }
 
