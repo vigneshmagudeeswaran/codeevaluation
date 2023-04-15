ece9d876493821b6bdb03a66d5491ad3e938276b
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-15 07:23:38 +0530

    renderLIST

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,9 +1,32 @@
 import React from "react";
 
 function NameList() {
-  const names = ["Bruce", "Clark", "Diana"];
-  const nameList = names.map((name) => <h2>{name}</h2>);
-  return <div>{nameList}</div>;
+  const persons = [
+    {
+      id: 1,
+      name: "Bruce",
+      age: 30,
+      skill: "React",
+    },
+    {
+      id: 2,
+      name: "Clarck",
+      age: 25,
+      skill: "React",
+    },
+    {
+      id: 3,
+      name: "Diana",
+      age: 28,
+      skill: "Vue",
+    },
+  ];
+  const personList = persons.map((person) => (
+    <h2>
+      I am {person.name}. I am {person.age} years old. I know{person.skill}
+    </h2>
+  ));
+  return <div>{personList}</div>;
 }
 
 export default NameList;
