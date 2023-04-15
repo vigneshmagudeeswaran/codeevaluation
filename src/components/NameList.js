28dd8fb97720fad63b8824fadce0183a2c747fe7
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-15 09:22:00 +0530

    Index_as_Key_Anti-pattern_2

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,30 +2,34 @@ import React from "react";
 import Person from "./Person";
 
 function NameList() {
-  const persons = [
-    {
-      id: 1,
-      name: "Bruce",
-      age: 30,
-      skill: "React",
-    },
-    {
-      id: 2,
-      name: "Clarck",
-      age: 25,
-      skill: "React",
-    },
-    {
-      id: 3,
-      name: "Diana",
-      age: 28,
-      skill: "Vue",
-    },
-  ];
-  const personList = persons.map((person) => (
-    <Person key={person.id} person={person}></Person>
-  ));
-  return <div>{personList}</div>;
+  const names = ["Bruce", "Clark", "Diana", "Bruce"];
+
+  const nameList = names.map((name, index) => <h2 key={index}>{name}</h2>);
+  return <div>{nameList}</div>;
+  //   const persons = [
+  //     {
+  //       id: 1,
+  //       name: "Bruce",
+  //       age: 30,
+  //       skill: "React",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Clarck",
+  //       age: 25,
+  //       skill: "React",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Diana",
+  //       age: 28,
+  //       skill: "Vue",
+  //     },
+  //   ];
+  //   const personList = persons.map((person) => (
+  //     <Person key={person.id} person={person}></Person>
+  //   ));
+  //   return <div>{personList}</div>;
 }
 
 export default NameList;
