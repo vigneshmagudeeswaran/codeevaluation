4b730a22f367d684bc949628ba2aeb4eb7169824
vigneshmagudeeswaran <m.vigneshviki1996@gmail.com>
2023-04-15 05:56:22 +0530

    using_conditionalANDternaryOperatorToPrintSpecificElement

---
+++ src/components/UserGreeting.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class UserGreeting extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: true,
+    };
+  }
+  render() {
+    // let message;
+    // if (this.state.isLoggedIn) {
+    //   message = <div>Welcome Vishwas</div>;
+    // } else {
+    //   message = <div>Welcome Guest</div>;
+    return this.state.isLoggedIn ? (
+      <div>Welcome Vishwas</div>
+    ) : (
+      <div>Welcome Guest</div>
+    );
+    //   <div>
+    //     <div>{message}</div>
+    //   </div>
+  }
+}
+
+export default UserGreeting;
